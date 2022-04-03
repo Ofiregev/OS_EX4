a82f1bee38e29669c8d9f39ed40f1e9a8de7aee5
RazGavrieli <razgavrieli@gmail.com>
2022-04-03 15:24:33 +0300

    added stack class, still needs dynamic resizing

--- stack.cpp
+++ stack.cpp
@@ -16,19 +16,66 @@
 #include <string>
 #include <iostream>
 #include <cstdlib>
+#define INIT_SIZE 5
+using namespace std;
+
+class stack {
+    string stack[INIT_SIZE]; 
+    int size = 5;
+    int pointer = 0;
+
+    public:
+    // stack() {
+
+    // }
+    bool push(string text) {
+        if (pointer==size-1) {
+            return false; 
+            // RESIZE THE STACK
+        }
+        stack[pointer++] = text;
+        return true;
+    }
+
+    string top() {
+        if (pointer == 0) {
+            return "STACK EMPTY!";
+        }
+        return stack[pointer-1];
+    }
+
+    bool pop() {
+        if (pointer == 0) {
+            return false; 
+        }
+        pointer--;
+        return true;
+    }
+};
+
+
+
+
 int main () {
     bool running = true;
+    stack stack;
     while (running) {
-        std::string command; 
-        std::cin >> command;
+        string command; 
+        cin >> command;
         if (command == "PUSH") {
-            std::string line;
-            std::getline(std::cin, line);
-            std::cout << line;
+            string line;
+            getline(cin, line);
+            if (!stack.push(line)) {
+                cout << "PUSH failed\n";
+            };
         } else if (command == "POP") {
-
+            if (!stack.pop()) {
+                cout << "POP failed\n";
+            } else {
+                cout << "POPPED\n";
+            }
         } else if (command == "TOP") {
-
+            cout << "OUTPUT: " << stack.top() << endl;
         } else if (command == "EXIT") {
             running = false;
         }
