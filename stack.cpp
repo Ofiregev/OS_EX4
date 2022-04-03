65f8da59a9674a6ab6a97465237bd65af56a7021
RazGavrieli <razgavrieli@gmail.com>
2022-04-03 14:46:36 +0300

    stack.cpp file added

---
+++ stack.cpp
@@ -0,0 +1,21 @@
+// global variable:
+// vector of strings, each string is 1024 bytes
+// 
+
+// PUSH
+// push pushs a new value into the stack
+
+
+// POP
+// pop returns and deletes the top value in the stack
+
+// TOP
+// top returns the top value in the stack
+
+
+int main () {
+    while (1) {
+        // get PUSH\POP\TOP
+    }
+
+}
